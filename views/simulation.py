9fd27d21da7280b7421bb2cb490aab83e1e7eae2
dkrivaa <dannykrivaa@hotmail.com>
2024-12-04 11:32:33 +0200

    Initial commit

--- views/simulation.py
+++ views/simulation.py
@@ -39,7 +39,7 @@ with st.container():
             st.switch_page('views/actions/wage_policies.py')
 
     if st.session_state['actions']:
-        st.page_link(label=':orange[Press] here :orange[to see all actions implemented]',
+        st.page_link(label=':orange[Actions implemented - ]:orange[Press] here',
                      page='views/actions/summary.py')
 
 
