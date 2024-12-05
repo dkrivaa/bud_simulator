222a01250d54218383d652e9b13de81dd3e7a16c
dkrivaa <dannykrivaa@hotmail.com>
2024-12-05 08:27:56 +0200

    Initial commit

--- views/compare.py
+++ views/compare.py
@@ -7,9 +7,9 @@ from functions.budget import list_of_takanot
 
 def compare_budgets(compare):
     var_dict = {
-        'Budget': 'הוצאה נטו',
-        'Manpower': 'שיא כח אדם',
-        'Amount': 'כמות'
+        'budget': 'הוצאה נטו',
+        'manpower': 'שיא כח אדם',
+        'amount': 'כמות'
     }
     var = var_dict[compare]
     df_original = st.session_state['df_original'].copy()
@@ -17,9 +17,7 @@ def compare_budgets(compare):
     df_compare = pd.merge(df_original[['קוד ושם תקנה', var]].rename(columns={var: 'Original Budget'}),
                           df[['קוד ושם תקנה', var]].rename(columns={var: 'Simulation Budget'}),
                           on=['קוד ושם תקנה'], how='outer')
-    df_compare = df_compare[(df_compare['Original Budget'].notna()) |
-                            (df_compare['Original Budget'] != 0) |
-                            (df_compare['Simulation Budget'].notna()) |
+    df_compare = df_compare[(df_compare['Original Budget'] != 0) &
                             (df_compare['Simulation Budget'] != 0)]
 
     for _, row in df_compare.iterrows():
@@ -49,9 +47,9 @@ st.divider()
 options = ['Budget', 'Manpower', 'Other Amounts']
 tab1, tab2, tab3 = st.tabs(options)
 with tab1:
-    compare_budgets('Budget')
+    compare_budgets('budget')
 with tab2:
-    compare_budgets('Manpower')
+    compare_budgets('manpower')
 with tab3:
-    compare_budgets('Amount')
+    compare_budgets('amount')
 
