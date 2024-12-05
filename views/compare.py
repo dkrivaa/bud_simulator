af7770df2b564bdf78a7ee65c19918e0d2b9aaf3
dkrivaa <dannykrivaa@hotmail.com>
2024-12-05 07:31:47 +0200

    Initial commit

--- views/compare.py
+++ views/compare.py
@@ -8,3 +8,11 @@ initial_check()
 # # Check that there are actions
 check_actions()
 
+
+st.title('Compare Budgets')
+st.page_link(page='views/simulation.py', label='Back to Simulator')
+st.divider()
+
+options = ['Budget', 'Manpower', 'Other Amounts']
+tab1, tab2, tab3 = st.tabs([options])
+
